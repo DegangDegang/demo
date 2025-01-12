81760f443bdb600f46fa648e006fe0745b314fd5
jongchan0109 <jongchan1009@naver.com>
2025-01-12 20:06:39 +0900

    feat: 게시글 도메인 구현

---
+++ src/main/java/com/example/demo/domain/post/domain/Post.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain.post.domain;
+
+import com.example.demo.domain.user.domain.User;
+import com.example.demo.global.database.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Post(Long id, String title, String content, User user) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+
+}
